999f7a9fd360f57d14fcb3c628b10511f7b92418
Krithikaperugu <81631590+Krithikaperugu@users.noreply.github.com>
2021-07-31 12:00:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,118 @@
+var audio1 = document.getElementById("myAudio");
+
+var audio2 = document.getElementById("myAudio2");
+
+var audio3 = document.getElementById("myAudio3");
+
+var audio4 = document.getElementById("myAudio4");
+
+var audio5 = document.getElementById("myAudio5");
+
+var audio6 = document.getElementById("myAudio6");
+
+var audio7 = document.getElementById("myAudio7");
+
+var audio8 = document.getElementById("myAudio8");
+
+
+function playSound(){
+	audio2.pause();
+	audio3.pause();
+	audio4.pause();
+	audio5.pause();
+	audio7.pause();
+	audio6.pause();
+	audio8.pause();
+	
+	
+	audio1.play();
+}
+ 
+
+function playSound2(){
+	audio1.pause();
+	audio3.pause();
+	audio4.pause();
+	audio5.pause();
+	audio7.pause();
+	audio6.pause();
+	audio8.pause();
+	audio2.play();
+
+}
+
+
+function playSound3(){
+	audio2.pause();
+	audio1.pause();
+	audio4.pause();
+	audio5.pause();
+	audio7.pause();
+	audio6.pause();
+	audio8.pause();
+	
+	audio3.play();
+}
+
+
+function playSound4(){
+	audio2.pause();
+	audio3.pause();
+	audio1.pause();
+	audio5.pause();
+	audio7.pause();
+	audio6.pause();
+	audio8.pause();
+	audio4.play();
+}
+
+function playSound5(){
+	audio2.pause();
+	audio3.pause();
+	audio4.pause();
+	audio1.pause();
+	audio7.pause();
+	audio6.pause();
+	audio8.pause();
+	
+	audio5.play();
+}
+ 
+
+function playSound6(){
+	audio1.pause();
+	audio3.pause();
+	audio4.pause();
+	audio5.pause();
+	audio7.pause();
+	audio1.pause();
+	audio8.pause();
+	audio6.play();
+}
+
+
+function playSound7(){
+	audio2.pause();
+	audio1.pause();
+	audio4.pause();
+	audio5.pause();
+	audio1.pause();
+	audio6.pause();
+	audio8.pause();
+	
+	audio7.play();
+}
+
+
+function playSound8(){
+	audio2.pause();
+	audio3.pause();
+	audio1.pause();
+	audio5.pause();
+	audio7.pause();
+	audio6.pause();
+	audio1.pause();
+	audio8.play();
+}
+
+
